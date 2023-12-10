c1eb13cb216e67f783a741ced53cfe475c1cadb7
Fran <124793609+franbrizuelab@users.noreply.github.com>
2023-12-10 08:52:02 +0800

    Create 1.2decla.h
    Just the header file

---
+++ 1.2decla.h
@@ -0,0 +1,10 @@
+#pragma once
+extern  struct part* head;
+void read_line(char* str, int n);
+struct part* find_part(int number);
+void insert();
+void search();
+void update();
+void display(); 
+#pragma once
+ 
